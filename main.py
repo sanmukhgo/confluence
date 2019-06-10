70f34ba028cd8465f8ff00f1f6a30513d9d87de5
sanmukhgo <sanmukh.s@greyorange.com>
2019-06-10 23:52:43 +0530

    tested on h tags

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import requests
 from bs4 import BeautifulSoup as bs
 
-s=bs(open("sample.html"),'html.parser')
-h1=s.h1.text.strip()
-h2=s.h1.text
-print(h1)
-print(h2)
+r=requests.get('https://stkapp.eu-gb.mybluemix.net/sample')
+html=r.text
+s=bs(html,'html.parser')
+h=s.find_all(['h1','h2','h3','h4','h5','h6'])
+spc="  "
+for i in h:
+    print(spc*(int(i.name[1])-1),end="")
+    print(i.text.strip())
 
