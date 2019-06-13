f4f15b50e5f8b3c8968fd028ef6ef9b113c753ef
Sanmukh Sinha <GREYORANGE\sanmukh.s@ggn000398.greyorange.com>
2019-06-13 14:45:52 +0530

    formatting

--- table_of_contents.py
+++ table_of_contents.py
@@ -5,25 +5,24 @@ import html5lib
 
 payload = {'os_username': 'sanmukh.s', 'os_password': 'Grey@1234'}
 url = 'https://work.greyorange.com/confluence/display/BS/40.+Inventory+Features'
+r = requests.post(url, data=payload)
+html = r.text
+soup = bs(html,'html5lib')
 
-r=requests.post(url, data=payload)
-html=r.text
-soup=bs(html,'html5lib')
+m = soup.find('div',{'id':'main'})
+content = m.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+spc = "   "   #tab space
 
-m=soup.find('div',{'id':'main'})
-content=m.find_all(['h1','h2','h3','h4','h5','h6'])
-spc="   "   #tab space
-
-print("TABLE OF CONTENTS:\n")
-print((content[0].text.strip())[4:])
-l=len(content)
+print ("TABLE OF CONTENTS:\n")
+print ((content[0].text.strip())[4:])
+l = len(content)
 
 for i in range(1,l):
-    hn1=int(content[i].name[1])
-    print(spc*(hn1-1),end='')       #puts indentation
-    print(content[i].text.strip())  #prints content
+    hn1 = int(content[i].name[1])
+    print (spc*(hn1-1),end='')       #puts indentation
+    print (content[i].text.strip())  #prints content
     try:
-        hn2=int(content[i+1].name[1])
+        hn2 = int(content[i+1].name[1])
     except:
         pass
     else:
