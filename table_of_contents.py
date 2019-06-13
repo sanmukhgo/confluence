9d81edacd480cb46bd7aca42981f9e02af40dfdf
Sanmukh Sinha <GREYORANGE\sanmukh.s@ggn000398.greyorange.com>
2019-06-13 10:37:18 +0530

    cleaned code

--- table_of_contents.py
+++ table_of_contents.py
@@ -5,6 +5,7 @@ import html5lib
 
 payload = {'os_username': 'sanmukh.s', 'os_password': 'Grey@1234'}
 url = 'https://work.greyorange.com/confluence/display/BS/40.+Inventory+Features'
+
 r=requests.post(url, data=payload)
 html=r.text
 soup=bs(html,'html5lib')
@@ -15,8 +16,18 @@ spc="   "   #tab space
 
 print("TABLE OF CONTENTS:\n")
 print((content[0].text.strip())[4:])
-for i in range(1,len(content)):
-    print(spc*(int(content[i].name[1])-1),end='')   #puts indentation
+l=len(content)
+
+for i in range(1,l):
+    hn1=int(content[i].name[1])
+    print(spc*(hn1-1),end='')   #puts indentation
     print(content[i].text.strip())  #prints content
-    if (i<len(content)-1) and (content[i].name[1]>content[i+1].name[1]):    #puts newlines
-        print()
+    try:
+        hn2=int(content[i+1].name[1])
+    except:
+        pass
+    else:
+        if (i<l-1) and (hn1>hn2):    #puts newlines
+            print()
+
+
