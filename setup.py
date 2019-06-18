6bfa4768a697e671f5762432758f39dc9438700f
sanmukhgo <sanmukh.s@greyorange.com>
2019-06-18 11:59:24 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ConfluenceWiki',
+      version='1.0',
+      description='A Webcrawler Project',
+      author='Sanmukh Sinha',
+      author_email='sanmukh.s@greyorange.com',
+      url='',
+      packages=['module'],
+      )
