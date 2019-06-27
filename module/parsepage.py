59ac63f8cf1cabb99d9f2896af015a08311d2e94
sanmukhgo <sanmukh.s@greyorange.com>
2019-06-27 13:22:44 +0530

    bug fixes

--- module/parsepage.py
+++ module/parsepage.py
@@ -1,13 +1,14 @@
 """ Parses the Inventory Features page as "soup". """
 
 from bs4 import BeautifulSoup as bs
+import reqpage
 import lxml
 import html5lib
 
 
 def _parse(location: str, parser: str):
     """
-    Returns parsed page at "location as a BeautifulSoup object, 
+    Returns parsed page at "location" as a BeautifulSoup object, 
     using the given parsers:
 
         lxml
@@ -16,9 +17,15 @@ def _parse(location: str, parser: str):
     """
     try:
         f = open(location, "r")
+    except:
+        reqpage.save_inventory()
+        return _parse(location,parser)
+
+    try:
         return bs(f.read(), parser)
     finally:
         f.close()
+    
 
 
 _html = "page/inventory.html"
